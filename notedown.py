ee0776a1fe10e012bf2619ce29c1c77f91d5e19b
Shawn Garbett <shawn@garbett.org>
2022-10-18 12:19:29 -0500

    Bit of progress towards first step of indexing local notes

--- notedown.py
+++ notedown.py
@@ -4,34 +4,51 @@ import sys
 import os
 import argparse
 import re
+import code
 
 quiet = False
 
 four_num = re.compile("\d\d\d\d")
 two_num  = re.compile("\d\d")
-md_re    = re.compile(".*\.md")
 
 def progress(msg):
     if not quiet: 
         print(msg, end="")
 
+def scan_file(file, date):
+    con = open(file, 'r')
+    lines = con.readlines()
+    con.close()
+    hdr1 = [i for i in range(0, len(lines)-1) if lines[i].startswith("# ")]
+    ends = [i-1 for i in hdr1]
+    return [[file,lines[hdr][2:-1],date] for hdr in hdr1]
+
 def scan_nd(path, yyyy, mm, dd):
     path = os.path.join(path, yyyy, mm, dd)
-    list(filter(md_re.fullmatch, os.listdir(path)))
+    files =  [file for file in os.listdir(path) if file.endswith(".md")]
+    results = [scan_file(os.path.join(path,file), "/".join([yyyy, mm, dd])) for file in files]  
+    return([item for sublist in results for item in sublist])
 
 def scan_dd(path, yyyy, mm):
-    progress(" "+mm) 
     days = list(filter(two_num.fullmatch, os.listdir(os.path.join(path, yyyy, mm))))
-    [scan_nd(path, yyyy, mm, dd) for dd in days]
+    days.sort()
+    results = [scan_nd(path, yyyy, mm, dd) for dd in days]
+    results = [item for sublist in results for item in sublist]
+    progress(" "+mm+": "+",".join(days)+f"---{len(results)} entries\n") 
+    return(results)
 
 def scan_mm(path, yyyy):
-    progress("Found " + yyyy + "\n")
+    progress("Found " + yyyy+"\n")
     months = list(filter(two_num.fullmatch, os.listdir(os.path.join(path, yyyy))))
-    [scan_dd(path, yyyy, mm) for mm in months] 
+    months.sort()
+    results = [scan_dd(path, yyyy, mm) for mm in months] 
+    return [item for sublist in results for item in sublist]
 
 def scan(path):
     years = list(filter(four_num.fullmatch, os.listdir(path)))
+    years.sort()
     results = [scan_mm(path, yyyy) for yyyy in years]
+    return [item for sublist in results for item in sublist]
 
 INVALID_PATH_MSG = "Error: Invalid path name. '%s' is not a valid path."
 
@@ -45,10 +62,11 @@ args = arg_parser.parse_args()
 if not os.path.isdir(args.path):
   print(INVALID_PATH_MSG%(args.path))
 
-quiet = args.quiet
+quiet = not (args.quiet == None)
 
 progress(f"Starting Indexing at {args.path}\n")
 
 index = scan(args.path)
 
+print("\n")
 print(index)
